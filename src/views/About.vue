9d5444ae2e8318d065fd1d6c98818608aa84d2b7
laveena95 <laveenalavee256@gmail.com>
2020-07-07 17:54:48 +0530

    basic website UI

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,15 @@
 <template>
   <v-content>
       <div class="staticHero">
-         jdkd
+         <v-img src="../assets/img/G2.jpg" style="max-height: 434px">
+          <v-row align="end" class="lightbox white--text pa-2 fill-height">
+            <v-col>
+              <v-container>
+                <div class="headline">ABOUT US</div>
+              </v-container>
+            </v-col>
+          </v-row>
+        </v-img>
       </div>
   </v-content>
 </template>
